8a96ca014bc5ba0205dc5a7450f4938964f8542e
JoaoCCTeixeira <joaoccaleixo@gmail.com>
2022-06-13 15:11:30 +0100

    fromCursor empregados_eventos

--- app/src/main/java/pt/ipg/organizadoreventos/empregados_eventos.kt
+++ app/src/main/java/pt/ipg/organizadoreventos/empregados_eventos.kt
@@ -1,6 +1,8 @@
 package pt.ipg.organizadoreventos
 
 import android.content.ContentValues
+import android.database.Cursor
+import android.provider.BaseColumns
 
 data class empregados_eventos (var id_empregados: Long, var id_eventos: Long, var id: Long = -1){
 
@@ -11,5 +13,21 @@ data class empregados_eventos (var id_empregados: Long, var id_eventos: Long, va
         valores.put(TabelaBDEmpregados_eventos.ID_EVENTOS, id_eventos)
 
         return valores
+    }
+
+    companion object {
+        fun fromCursor(cursor: Cursor): empregados_eventos {
+            val posId = cursor.getColumnIndex(BaseColumns._ID)
+
+            val posIdEmpregados = cursor.getColumnIndex(TabelaBDEmpregados_eventos.ID_EMPREGADOS)
+            val posIdEventos = cursor.getColumnIndex(TabelaBDEmpregados_eventos.ID_EVENTOS)
+
+            val id = cursor.getLong(posId)
+
+            val idEmpregados = cursor.getLong(posIdEmpregados)
+            val idEventos = cursor.getLong(posIdEventos)
+
+            return empregados_eventos(idEmpregados,idEventos, id)
+        }
     }
 }
