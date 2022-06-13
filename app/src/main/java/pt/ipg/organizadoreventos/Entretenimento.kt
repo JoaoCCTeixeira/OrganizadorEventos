9ccbd535957b4bdbfae9e60c5573fa4da5f2d2e3
JoaoCCTeixeira <joaoccaleixo@gmail.com>
2022-06-13 01:34:52 +0100

    id entretenimento

--- app/src/main/java/pt/ipg/organizadoreventos/Entretenimento.kt
+++ app/src/main/java/pt/ipg/organizadoreventos/Entretenimento.kt
@@ -2,7 +2,7 @@ package pt.ipg.organizadoreventos
 
 import android.content.ContentValues
 
-class Entretenimento (var tema: String){
+class Entretenimento (var tema: String, var id: Long = -1){
 
     fun toContentValues() : ContentValues {
         val valores = ContentValues()
