7664f029f967f9dd8295962746006b236c372baf
JoaoCCTeixeira <joaoccaleixo@gmail.com>
2022-06-13 01:35:08 +0100

    id eventos

--- app/src/main/java/pt/ipg/organizadoreventos/Eventos.kt
+++ app/src/main/java/pt/ipg/organizadoreventos/Eventos.kt
@@ -2,7 +2,7 @@ package pt.ipg.organizadoreventos
 
 import android.content.ContentValues
 
-data class Eventos (var data: String, var preco: Long, var localizacao: String, var nome: String, var id_entretenimento: Long){
+data class Eventos (var data: String, var preco: Long, var localizacao: String, var nome: String, var id_entretenimento: Long, var id: Long = -1){
 
     fun toContentValues() : ContentValues {
         val valores = ContentValues()
