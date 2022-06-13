567c69fd52c9b6ab42f7da7d1bee549e2fdaefe7
JoaoCCTeixeira <joaoccaleixo@gmail.com>
2022-06-13 01:09:52 +0100

    Criancao do open helper

---
+++ app/src/main/java/pt/ipg/organizadoreventos/BDEventosOpenHelper.kt
@@ -0,0 +1,28 @@
+package pt.ipg.organizadoreventos
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class BDEventosOpenHelper (context: Context?) : SQLiteOpenHelper(context, NOME, null, VERSAO) {
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        requireNotNull(db)
+
+        TabelaBDConvidados(db).cria()
+        TabelaBDEmpregados(db).cria()
+        TabelaBDEntretenimento(db).cria()
+        TabelaBDEventos(db).cria()
+        TabelaBDConvidados_eventos(db).cria()
+        TabelaBDEmpregados_eventos(db).cria()
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+    }
+
+    companion object {
+        const val NOME = "eventos.db"
+        private const val VERSAO = 1
+    }
+
+}
